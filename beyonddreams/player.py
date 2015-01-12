677b860be498c809d07307ac778bad05e8058527
Speks of Dust <speksofdust@users.noreply.github.com>
2015-01-11 23:35:53 -0500

    Update player.py

--- beyonddreams/player.py
+++ beyonddreams/player.py
@@ -20,22 +20,26 @@ def indexed_chardict(chars):
     for i in enumerate(tuple(chars)): d[i[0]] = i[1]
     return d
 
-class CharRoster:
-    def __init__(self, chars={}):
-        if isinstance(chars, dict): self._chars = chars
-        else:
-            self._chars = indexed_chardict(chars):
-            
+
+class CharStorageBC:
+    
+    def __eq__(self, x):        return self._chars == x
+    def __ne__(self, x):        return self._chars != x
+    def __str__(self):          return str(self._chars)[1:-1]
+    def __repr__(self):         return repr(self._chars)
     def __getitem__(self, i):   return self._chars[i]
+    def __bool__(self):         return len(self._chars) != 0
     def __len__(self):          return len(self._chars)
-    def __str__(self):          return str(tuple(iter(self._chars)))[1:-1]
-    def __repr__(self):         return str(tuple(iter(self._chars)))
+    def __iter__(self):         return iter(self._chars)
+    def __reversed__(self):     return reversed(self._chars)
     
-    def __iter__(self): 
-        return iter(self._chars[i] for i in range(0, len(self._chars)))
-    
-    def __reversed__(self): 
-        return iter(self._chars[i] for i in range(len(self._chars)-1, -1, -1))
+    def index(self, n):
+        return self._chars.index(n)
+        
+
+class CharRoster(CharStorageBC):
+    def __init__(self, chars=[]):
+        self._chars = list(chars)
     
     def idx_from_name(self, name):
         for i in self._chars:
@@ -45,27 +49,67 @@ class CharRoster:
         tmp = self[idx_a]
         self[idx_a] = self[idx_b] 
         self[idx_b] = tmp
+
+
+class Party(CharStorageBC):
+    def __init__(self):
+        self._items = CharRoster()
+        self._current = None    # stored as an index (int)
         
-    # ---- game only ----------------------------------------------------- #
+    def next(self):
+        """Set the next item as the current item."""
+        self._current = self.next_index()
+        
+    def prev(self):
+        """Set the previous item as the current item."""
+        self._current = self._prev_index()
+        
+    def get_next(self):
+        """Return the next item."""
+        return self._items[self.next_index()]
+        
+    def get_prev(self):
+        """Return the previous item."""
+        return self._items[self.prev_index()]
+    
+    def next_index(self):
+        """Return the index of the next item."""
+        try: 
+            if self._current < len(self._items): return self._current + 1
+            return 0
+        except: return None
+
+    def prev_index(self):
+        """Return the index of the previous item."""
+        try:
+            if self._current == 0: return len(self._items)
+            return self._current - 1
+        except:
+            return None
+
     def get_alive(self):
         """Return an iterator of all characters still alive."""
-        return iter(i for i in self._chars if i.is_alive())
+        return iter(i for i in self._items if i.is_alive())
         
     def get_critical(self):
         """Return an iterator of all characters with critical health levels."""
-        return iter(i for i in self._chars if i.is_critical())
+        return iter(i for i in self._items if i.is_critical())
 
 
 class Player:
+    _type = ""
     def __init__(self):
-        self._chars =   CharRoster()
+        self._party =   Party()
         self._pid =     0   # always 0 if not in online game
         
     @property
-    def chars(self):
+    def party(self):
         """The character roster for this player."""
-        return self._chars
+        return self._party
         
+        
+class AIPlayer(Player):
+    _type = "AI"
     
     
         
