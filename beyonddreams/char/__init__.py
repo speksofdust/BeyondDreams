6512eebf818a9a38575307976fd072ca04b03308
Speks of Dust <speksofdust@users.noreply.github.com>
2015-02-03 02:35:39 -0500

    Update __init__.py

--- beyonddreams/char/__init__.py
+++ beyonddreams/char/__init__.py
@@ -27,31 +27,67 @@ from attribs import Equip
 from attribs import Stats
 from attribs import StatusEffects
         
+
+class BDCharException(Exception): pass
+        
+        
 def _initca(char, v, attribcls):
     # helper for initalizing each charattirb type
     if isinstance(v, attribcls): return charattrib
     elif v is None: return charratrib(char)
     else: return charattrib(char, *v)
         
+def finalize_char(char, player):
+    """Finalize changes, called when character editing (post creation)
+        is done."""
+    try: char._finalize(player)
+    except: raise BDCharException("Character '{}' already finalized".format(char)))
+        
+        
+BODY =          "body"
+INVENTORY =     "inventory"
+EQUIP =         "equip"
+WALLET =        "wallet"
+STATS =         "stats"
+STATUSES =      "statuses"
+RESISTANCES =   "resistances"
+CHARATTRIB_NAMES = BODY, INVENTORY, EQUIP, WALLET, STATS, STATUSES, RESISTANCES
+
 class Char:
     _type = "normal"
-    CHARATTRIB_NAMES = "body", "inventory", "equip", "wallet", "stats", "statuseffects"
-    def __init__(self, player, base=None, body=None, inventory=None, equip=None, 
+    def __init__(self, base=None, body=None, inventory=None, equip=None, 
         wallet=None, stats=None, statuseffects=None):
-        self._player =          player
+        self._finalized =       False
+        self._player =          None
         self._base =            base
-        self._body =            _initca(self, body, Body)
-        self._inventory =       _initca(self, inventory, Inventory)
-        self._equip =           _initca(self, equip, Equip)
-        self._wallet =          _initca(self, wallet, Wallet)
-        self._stats =           _initca(self, stats, Stats)
-        self._statuseffects =   _initca(self, statuseffects, StatusEffects)
-        import random
-        self._seed =        random.uniform(0.0, 99999.9)
-        
-        
-    def __bool__(self): return True
-    def __hash__(self): return hash(id(self), self._seed)
+        self._items = {
+            BODY:   Body(self)
+            }
+    
+    def _finalize(self, player):
+        if not self._finalized
+            import random
+            self._seed =    random.uniform(0.0, 99999.9)
+            self._player =  player
+            self._items = {
+                BODY:           self._items[BODY],
+                INVENTORY:      Inventory(self),
+                EQUIP:          Equip(self),
+                WALLET:         Wallet(self),
+                STATS:          Stats(self),
+                STATUSES:       Statuses(self),
+                RESISTANCES:    None
+                }
+        else: raise BDCharException
+    
+    def __dict__(self):         return self._items.copy()
+    def __iter__(self):         return iter(self._items)
+    def __len__(self):          return len(self._items)
+    def __str__(self):          return str(self._items)[1:-1]
+    def __repr__(self):         return repr(self._items)
+    def __bool__(self):         return True
+    def __hash__(self):         return hash(id(self))
+    def __getitem__(self, i):   return self._items[i]
         
     def __eq__(self, x):
         if isinstance(x, type(self)): return x is self
@@ -61,6 +97,36 @@ class Char:
         if isinstance(x, type(self)): return x is not self
         raise TypeError("Cannot compare type: '{type(self)}' to '{type(x)}'")
 
+    @property
+    def party(self):
+        """The party this char is in."""
+        return self._player._items[PARTY]
+
+    @property
+    def base(self):
+        """Base values for this character."""
+        return self._base
+
+    @property
+    def body(self):
+        """This characters body attributes."""
+        return self._items[BODY]
+
+    @property
+    def wallet(self):
+        """This characters wallet."""
+        return self._items[WALLET]
+
+    @property
+    def inventory(self):
+        """This characters inventory."""
+        return self._items[INVENTORY]
+
+    @property
+    def equip(self):
+        """This characters equipment."""
+        return self._items[EQUIP]
+        
     def is_player(self):
         """True if this char is controlled by the "player" object
             on the local machine."""
@@ -68,41 +134,23 @@ class Char:
 
     def is_alive(self):
         """True if this character is alive."""
-        return self._stats.health != 0
+        if self._items[STATS][HP] == 0: return False
+        return True
 
     def is_critical(self):
         """True if this characters health level is in the critical range."""
-        return 0 < self._stats.health <= 20
-
+        return self._items[STATS][HP].is_critical()
+        
     @property
-    def party(self):
-        """The party this char is in."""
-        return self._player.party
-
-    @property
-    def defaults(self):
-        """Default values for this character."""
-        return self._defaults
-
-    @property
-    def body(self):
-        """This characters body attributes."""
-        return self._body
-
-    @property
-    def wallet(self):
-        """This characters wallet."""
-        return self._wallet
-
-    @property
-    def inventory(self):
-        """This characters inventory."""
-        return self._inventory
-
-    @property
-    def equip(self):
-        """This characters equipment."""
-        return self._equip
+    def famtypes(self):
+        """Return family types for this character."""
+        return ()
+        
+    def is_undead(self):
+        """True if this character is an undead type or has zombie status."""
+        return ("zombie" in self._items[STATUSES]["bools"] or 
+            "zombie" in self.famtypes)
+        
 
         
 class NPC(Char):
