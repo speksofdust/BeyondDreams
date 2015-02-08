9833861209f0ca39039a12f0aa6883196aa6f336
Speks of Dust <speksofdust@users.noreply.github.com>
2015-02-08 11:23:12 -0500

    Create weapons.py

---
+++ beyonddreams/item/weapons.py
@@ -0,0 +1,38 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+#     This program is free software: you can redistribute it and/or modify     #
+#     it under the terms of the GNU General Public License as published by     #
+#     the Free Software Foundation, either version 3 of the License, or        #
+#     (at your option) any later version.                                      #
+#                                                                              #
+#     This program is distributed in the hope that it will be useful,          #
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the             #
+#     GNU General Public License for more details.                             #
+#                                                                              #
+#     You should have received a copy of the GNU General Public License        #
+#     along with this program. If not, see <http://www.gnu.org/licenses/>.     #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+from itemtypes import ItemType
+from itemtypes import regitem
+
+
+class WeaponType(ItemType):
+    """Base class for weapon types."""
+    CATTYPE =      "weapon"
+    BUNDLESIZE =   1
+    
+    
+class Ammo(WeaponType):
+    BUNDLESIZE =  25
+    weapontypes = () # weapontypes usable in
+    
+    
+class DefensiveType(WeaponType):
+    pass
+    
+
+class WeaponAccessories(WeaponType):
+    pass
