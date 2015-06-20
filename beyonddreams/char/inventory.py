e389f4f6cdc30d3c316d68b78b4b5d52a3993cd8
Speks of Dust <speksofdust@users.noreply.github.com>
2015-06-19 23:20:47 -0400

    fixes

--- beyonddreams/char/inventory.py
+++ beyonddreams/char/inventory.py
@@ -39,8 +39,8 @@ class InventoryItem:
     # Note: AttributeError will be raised if comparing an item that is not an
     #   instance of InventoryItem (doesnt have a qty attrib)
     #   to compare item only use the item property
-    def __eq__(self, x): return (self._item == x._item and self.qty == x.qty) 
-    def __ne__(self, x): return (self._item != x._item or self.qty != x.qty) 
+    def __eq__(self, x): return (self._item == x._item and self.qty == x.qty)
+    def __ne__(self, x): return (self._item != x._item or self.qty != x.qty)
 
     # lt, gt, ge, le compare qty -- both items must be the same
     def __lt__(self, x):
@@ -102,70 +102,70 @@ class InvItemBundled(InventoryItem):
         return self._qty
 
 
-class Pocket:
-    __slots__ = "_inventory", "_items", "_pockettype"
+class Pocket(list):
+    __slots__ = "_inventory", "_pockettype"
     def __init__(self, inventory, pockettype, items=[]):
         self._inventory =   inventory
         self._pockettype =  pockettype
-        self._items =       list(items)
+        self = list(items)
 
-    def __iter__(self, x):      return iter(self._items)
-    def __repr__(self):         return str(self._items)
-    def __len__(self):          return len(self._items)
-    def __contains__(self, x):  return any(x == (i or i.name) for i in self._items)
+    def append(self): raise NotImplementedError
+    extend = insert = pop = append
+
+    def __contains__(self, x):  return any(x == (i or i.name) for i in self)
 
     # Note: Pocket x Pocket comparison must compare pockettype
     def __eq__(self, x):
         if isinstance(x, Pocket):       # Pocket x Pocket comp
             # must be exact match
-            return (self._pockettype != x._pockettype and self._items != x._items)
-        try: return x == self._items    # Pocket x OtherType comp
+            return (self._pockettype != x._pockettype and self != x._items)
+        try: return x == self    # Pocket x OtherType comp
         except: raise TypeError(
             "Cannot compare type: {} to Inventory 'Pocket' type.".format(type(x))
-        
+
     def __ne__(self, x):
         if isinstance(x, Pocket):       # Pocket x Pocket comp
             # any relavent difference returns False
-            if self._pockettype == x._pockettype:   return self._items != x._items)
+            if self._pockettype == x._pockettype:   return self != x._items)
             return False
-        try: return x != self._items    # Pocket x OtherType comp
+        try: return x != self    # Pocket x OtherType comp
         except: raise TypeError(
             "Cannot compare type: {} to Inventory 'Pocket' type.".format(type(x))
 
     def _additem(self, item):
-        for i in self._items:
+        for i in self:
             if i == item:
                 if isinstance(i, InvItemBundled):
                     i._qty += item.qty
                 elif isinstance(i, InvItemBundled):
                     item._qty += i.qty
-                    self._items[i] = item
+                    self[i] = item
                 else:
-                    self._items[i] = InvItemBundled(i, i.qty + item.qty)
-            return self._items[i]
-        self._items.append(item)
-        return self._items[-1]
+                    self[i] = InvItemBundled(i, i.qty + item.qty)
+            return self[i]
+        self.append(item)
+        return self[-1]
 
     @property
     def pockettype(self):
-        """The type of pocket. This determines what item types get 
+        """The type of pocket. This determines what item types get
             stored in this pocket."""
         return self._pockettype
-        
+
     def itemnames(self):
         """Return an iterator of the names of all items in this pocket."""
-        return iter(i.name for i in self._items))
-        
+        return iter(i.name for i in self))
+
     def difference(self, x):
         """Return all items in x but not in this pocket."""
-        return set(self._items).difference(x)
-    
+        return set(self).difference(x)
+
     def similar(self, x):
         """Return all items in this pocket and x."""
-        return set(self._items).intersection(x)
+        return set(self).intersection(x)
 
     def search_tags(self, include=None, exclude=None):
-        """Yield items with all tags in include 
+        """Yield items with all tags in include
             and none of the tags in exclude"""
         raise NotImplementedError
         # TODO
@@ -180,15 +180,15 @@ class Inventory(CharAttrib):
             Pocket(self, WEARABLES),
             Pocket(self, WEAPONS),
             )
-    
-    def __str__(self):  return tuple(repr(i) for i in self._pockets)
-    __repr__ = __str__    
-    
+
+    def __str__(self):  return str(self._pockets)
+    __repr__ = __str__
+
     def add_item(self, i):
         if isinstance(i, InventoryItem):
             self._pockets[i.itemdata.pockettype]._additem(i)
         raise TypeError("Invalid item type for inventory.")
-    
+
     def consumables(self):
         """Consumable items."""
         return self._pocket[0]
