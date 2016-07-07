c332eaf4ec2d9c8a9a65d5eee294827fbc76cafd
Speks of Dust <speksofdust@users.noreply.github.com>
2016-07-07 19:35:13 -0400

    fixes

--- beyonddreams/item/item.py
+++ beyonddreams/item/item.py
@@ -17,6 +17,8 @@
 
 from .core.bdtype import BDTaggedType
 from .core.bdtype import BDTypeDict
+from .bdobj import StorableObj
+from .bdobj import BDObjSet
 
 
 def regitem(i):
@@ -32,9 +34,9 @@ def _iterbycattype(ob, cattype):
     return iter(i for i in t if ob.CATTYPE == cattype)
 
 
-class ItemDict(BDTypeDict):
+class ItemDict():
     CATTYPE = ""
-    __slots__ = BDTypeDict.__slots__ + "_sets"
+    __slots__ = "_items", "_sets"
     def __init__(self):
         self._items = {}
         self._sets = set()
@@ -50,87 +52,35 @@ class Items:
         self.wearables =    WearableTypeDict()
 
 
-class ItemType(BDTaggedType):
+class ItemType(StorableObj):
     """Base class for all item types."""
     _bundleable =   False
-    CATTYPE =       ""    # Primary catagory type (CONSUMABLE, WEAPON, etc.)
     _classifiers =  ()
-    _typename =     ""    # name for this item type -- not for primary types
-    _typedesc =     ""
     ATTRIBS =       ()    # attribs for items when in inventory
     ALL_TYPE_ATTRIBS = () # all possible attribs for this type
-    # ---- Minimum worth values ------------------------------------------ #
-    _min_resale_value = 1 # <-- must be <= all other 'worth' 'values'
-    _min_pawn_value = 1   #
-    _min_trade_value = 1  #
-    # ---- Resale/trade attribs ------------------------------------------ #
-    _pawnable = True
-    _tradable = True
-    _resale_shoptypes = () # shoptypes that will buy this
-    # ---- stuff for consumables, weapons, and wearables only ------------ #
-    _bwt =          1.0   # base weight must be (float)
-    _slotsize =     1
-    _itemset =      None
-
-    def __init__(self, name, tags=(), item_sets=()):
-        BDTaggedType.__init__(self, name=name, tags=tags)
-        self._item_sets = item_sets
-
-    @property
-    def typename(self):
-        """The name of this item type."""
-        if self._typename: return self._typename
-        return self.CATTYPE
 
     def typetags(self):
         """Yield all type tags for this."""
-        yield self.CATTYPE
+        yield self._cattype
         for i in self._classifiers: yield i
         if (self._typename and typename not in self._classifiers):
             yield self._typename
 
-    def is_pawnable(self):
-        """True if this item can be sold to a pawn shop."""
-        return self._pawnable
-
-    def is_tradable(self):
-        """True if this item can be traded to another player or npc."""
-        return self._tradable
-
-    def is_sellable(self, buyer=None):
-        """True if this itemtype can be sold to the given buyer. If buyer is
-        'None', then returns True if this itemtype is allowed to be sold."""
-        if (buyer is None or buyer.is_player):
-            return self.tradable()
-        else:
-            try:
-                if buyer.storetype() == 'pawn': return self.is_pawnable()
-                else: return buyer.storetype() in self._resale_shoptypes
-            except:
-                return False
-
     def tags(self):
         for i in self._tags: yield i
         for i in self.typetags(): yield i
-        for i in self._subcattype_tags: yield i
 
     def is_bundable(self):
         """True if this item can be bundled."""
         return (self._bundleable and self._max_bundlesize > 1)
 
-    def is_in_set(self):
-        """True if this item is part of a set."""
-        return self._itemset is not None
-
 
 class BundableItemType(ItemType):
     """Item type with support for bundling."""
-    _bundleable = True
+    _bundlable = True
     ABS_MAX_BUNDLE =    99
     _max_bundlesize =   1   # max num items grouped per bundle
 
 
-class ItemSet(BDTaggedType):
+class ItemSet(BDObjSet):
     """Data Storage class for sets of items"""
-    def __init__(self, set_name, tags=()):
-        BDTaggedType.__init__(self, name=set_name, tags=tags)
