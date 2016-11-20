b7215353fd866e0b4165974bfce9e348cb498b53
Speks of Dust <speksofdust@users.noreply.github.com>
2016-11-20 13:14:15 -0500

    updates & fixes

--- beyonddreams/furn/furnature.py
+++ beyonddreams/furn/furnature.py
@@ -18,57 +18,159 @@
 from bdobj import Sittable
 from bdobj import Storage
 from bdobj import BDObj
+from bdobj import ObMeta
 
-_FURNATURE_IAS = ('move',)
+LAY =       100
+SIT =       101
+MOVE =      200 # move object
+PICKUP =    201 # pickup object
+SWITCH =    300 # switch on/off etc
+OPENCLOSE = 388 # open/close door etc
+PUSH =      389
+PULL =      390
+
+#placement
+FLOOR =     0
+WALL =      1
+CEILING =   2
+ON_OBJECT = 3
+
+SEATING = "seating"
+BEDDING = "bedding"
+LIGHTING = "lighting"
+DECORATIVE = "decorative"
+
+class FurnMeta(ObMeta):
+    ptype = "furnature"
+    __slots__ = ObMeta.__slots__ + "base_interactions", "default_placement"
+    def __init__(self, name, subcats=(), base_interactions=(), desc=""):
+        super().__init__(name, subcats, desc)
+        self._base_interactions = base_interactions
 
 
 class Furnature(BDObj):
-    _object_type = 'furnature'
-    _base_desc = """ """
-    _base_interactions = []
-
-
-class StorageFurnature(Furnature, _Storage) pass
-class SittableFurnature(Furnature, Sittable): pass
-class WallHanging(Furnature): pass
+    _ob_type = 'furnature'
+    _default_placement = FLOOR
 
 
 # ---- Furnature types ------------------------------------------------------- #
 class Bed(Furnature):
-    _typename = 'bed'
+    meta = FurnMeta("bed",
+        subcats=(BEDDING,),
+        base_ias=(LAY, SIT, MOVE),
+        desc=""
+        )
 
 
-class Chair(SittableFurnature):
-    _typename = 'chair'
+class Chair(Furnature):
+    meta = FurnMeta('chair',
+        subcats=(SEATING,),
+        base_ias=(SIT, MOVE),
+        desc=""
+        )
 
 
-class Stool(SittableFurnature):
-    _typename = 'stool'
+class Stool(Furnature):
+    meta = FurnMeta('stool',
+        subcats=(SEATING,),
+        base_ias=(SIT, MOVE),
+        desc=""
+        )
 
 
-class Couch(SittableFurnature):
-    _typename = 'couch'
+class LoveSeat(Furnature):
+    meta = FurnMeta('loveseat',
+        subcats=(SEATING,),
+        base_ias=(SIT, MOVE),
+        desc=""
+        )
 
 
-class Chest(StorageFurnature):
-    _typename = "chest"
+class Couch(Furnature):
+    meta = FurnMeta('couch',
+        subcats=(SEATING,),
+        base_ias=(LAY, SIT, MOVE),
+        desc=""
+        )
+
+
+class Chest(Furnature):
+    meta = FurnMeta('couch',
+        subcats=(STORAGE,),
+        base_ias=(SIT, OPENCLOSE, MOVE),
+        desc="")
     _max_slots = 15
 
 
-class Trunk(StorageFurnature):
-    _typename = "trunk"
+class Trunk(Furnature):
+    meta = FurnMeta('couch',
+        subcats=(STORAGE,),
+        base_ias=(SIT, OPENCLOSE, MOVE),
+        desc="")
     _max_slots = 20
 
 
+class Dresser(Furnature):
+    meta = FurnMeta('dresser',
+        subcats=(STORAGE,),
+        base_ias=(MOVE,),
+        desc="")
+    _max_slots = 15
+
+
 class Table(Furnature):
-    _typename = "table"
+    meta = FurnMeta('table',
+        subcats=(),
+        base_ias=(SIT, MOVE),
+        desc="")
 
 
 class Rug(Furnature):
-    _typename = "rug"
+    meta = FurnMeta('rug',
+        subcats=(),
+        base_ias=(SIT, LAY, MOVE),
+        desc="")
 
 
-class Frame(WallHanging):
-    _typename = "frame"
-    def __init__(self, image):
+class Frame(Furnature):
+    meta = ObMeta("frame",
+        subcats=(DECORATIVE),
+        base_ias=(MOVE,),
+        desc=""
+        )
+    _default_placement = WALL
+    def __init__(self, image=None):
         self._image = image
+
+
+class Lighting(Furnature):
+    pass
+
+
+class TableLamp(Lighting):
+    meta = FurnMeta('floor lamp',
+        subcats=(LIGHTING,),
+        base_ias=(SWITCH, MOVE),
+        desc=""
+        )
+
+
+class FloorLamp(Lighting):
+    meta = FurnMeta('floor lamp',
+        subcats=(LIGHTING,),
+        base_ias=(SWITCH, MOVE),
+        desc=""
+        )
+
+FURNISHING_TYPES = {
+    "chair":    Chair,
+    "couch":    Couch,
+    "loveseat": LoveSeat,
+    "table":    Table,
+    "trunk":    Trunk,
+    "chest":    Chest,
+    "rug":      Rug,
+    "frame":    Frame,
+    "table lamp": TableLamp,
+    "floor lamp": FloorLamp,
+}
