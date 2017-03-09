d593273c308c5b5f7771d9d4bd6f545f8f056c3b
Speks of Dust <speksofdust@users.noreply.github.com>
2017-03-09 04:30:08 -0500

    updated char, deleted chardata

--- beyonddreams/char/char.py
+++ beyonddreams/char/char.py
@@ -15,321 +15,262 @@
 #                                                                              #
 # ---------------------------------------------------------------------------- #
 
-from .bd import session
-import pdict
-from pdict import *
-
-from beyonddreams.core.baseclasses import BDDataDict
-from .vident import VIDENT_TYPES
-from .game.location import Visited
-from .game.planes import PLANES
-from stats import Stats
-from statuses import Statuses
-import cflags
-from chardictkwds import *
-
+from game.data import gamedata
 
 CHAR_SPECIALFLAGS = ('VISITED_NOMEM', 'RELATIONSHIPS_OFF')
-
 
 def _boolkw(char, k, **kwargs):
     if k in kwargs: char[k] = bool(kwargs[k])
 
+# will replace char.py
+CHARID_LEN = 30
+
+statuses_dict = {
+        # physical
+        "frozen":       0,   # bool
+        "frostbite" :   0,
+        "burn" :        0,
+        "numb" :        0,
+        "stun" :        0,
+        "poisoning" :   0,
+        "bleed" :       0,
+        "petrify":      0, # turned to stone
+
+        # mental
+        "blind" :       0,
+        "drunk" :       0,
+        "dumb" :        0,
+        "confusion" :   0,
+        #"hallucination": 0,
+
+        # transform
+        "zombie" :      0,
+        "mutagen" :     0,
+
+        # specials
+        "immunull":     0,  # bool
+        "immundown":    0,
+        }
+
+stats_dict = {
+    "phys-energy":      100,
+    "mental-energy":    100,
+    "health":           100,
+}
+
+monster_datadict = {
+    "temp":         {},
+    "temp-flags":   [],
+    "flags":        [],
+    "KO-flags":     [],
+    "base":         "",
+}
+
+wildlife_datadict = {
+    "temp":         {},
+    "temp-flags":   [],
+    "flags":        [],
+    "KO-flags":     [],
+    "base":         "",
+}
+
+char_datadict = {
+    "temp":         {}, # non-writable stuff
+    # flags
+    "temp-flags":   [], # non-writable
+    "flags":        [],
+    "KO-flags":     [], # cleared after revived|re-spawn
+    "marker-flags": [0, 0], # added/updated after significant points
+    # base data
+    "base":         "",
+
+    # name
+    "name": {
+        "first":        "",
+        "last":         "",
+        "middle":       "",
+        "nick":         ""
+        },
+    "location": {
+        'region':       0,
+        },
+
+    "bodybase":     {
+        "type":         0,
+        },
+
+    "body-current": {
+
+        },
+
+    # inventory & wallet, etc.
+    "cash":         [0],
+    "coupons":      [],
+    "cards":        [],
+    "bag":          "default",
+    "consumables":  [],
+    "weapons":      [],
+    "wearables":    [],
+    "keyitems":     [],
+
+    # equip
+    "handedness":   0,  # 0-left, 1-right, 2-ambidextrous
+    "equip":    {
+        },
+}
+
+
+def _clear_temps(d):
+    for i in gamedata[d]:
+        try: del gamedata[d][i]['temp']
+        except: KeyError
+
+def _update_chars(d):
+    for i in gamedata[d]: update_char(gamedata[d][i])
 
-class Char(pdict.PDict):
-    """Character data storage class."""
-    _ident = VIDENT_TYPES["char"]
-    __slots__ = pdict.PDict.__slots__ + ('_gamechar', '_controller',
-        '_specialflags', '_tempflags')
-    def __init__(self, data={}, parseinit=True, **kwargs):
-        # Non-Writable values
-        self._gamechar = False
-        self._controller = None # AI, player, etc. # needed for gamechar only
-        self._specialflags = set()
-        self._tempflags = set()
-
-        # parse kwargs
-        _boolkw(self, 'gamechar', kwargs)
-        _boolkw(self, NPC, kwargs)
-        if 'specialflags' in kwargs:
-            self._specialflags = set(kwargs['specialflags'])
-
-        super().__init__(data=data, parseinit=parseinit, *args, **kwargs)
-
-        # special flags/gamedata constflags stuff
-        if ('VISITED_OFF' not in self._specialflags or
-            gamedata['constflags']):
-                self[VISITED] = Visited()
-
-        #if ('RELATIONSHIPS_OFF' not in self._specialflags or
-        #    gamedata['constflags']):
-
-
-        # TODO validate/assign charid
-
-        if self._gamechar: # add to gamechars
-            gamedata['chars'][self[CHARID]] = self
-
-    def _defaultdict(self):
-        return {
-                BASE:           None,
-                NPC:            False,    # True if NPC
-                CHARID_LOCAL:   0,
-                CHARID:         0,         # server assigned charid
-                PARTYID:       -1,
-                HANDEDNESS:     0,
-                ALLIANCE:       'none',
-                GUILD:          'none',
-                EQUIP:          Equip(self),
-                INVENTORY:      Inventory(self),
-                STATS:          Stats(self, kwargs),
-                STATUSES:       Statuses(self, kwargs),
-                VISITED:        False,
-                #### partially implemented ####
-                #RELATIONSHIPS = {},
-                #### not yet implemented ####
-                #PERSONALITY:  Personality(),
-                #MOOD:         Mood(),
-
-                # -- flags -- #
-                'dflags':       cflags.DFlags(), # died flags
-                'ond-flags':    cflags.CharFlags(), # on died flags
-                'onr-flags':    cflags.CharFlags(), # on revive flags
-
-                PLANE:        ['mortal', 'none'],
-                }
-
-    def _parseinit(self):
-        # convert json data to proper classes
-        init_from_key_cls_pairs_child(self, (
-            (EQUIP, Equip),
-            (INVENTORY, Inventory),
-            (STATS, Stats),
-            (STATUSES, Statuses),
-            #(PERSONALITY, Personality),
-            #(MOOD, Mood),
-            ))
-        # convert flags to sets
-        self['dflags'] = cflags.DFlags(self['dflags'])
-        init_from_key(self, 'ond-flags', cflags.CharFlags)
-        init_from_key(self, 'onr-flags', cflags.CharFlags)
-
-
-    # ---- Quick access to common stuff ---------------------------------- #
-    @property
-    def handedness(self):
-        """This character's primary hand. (0=Left, 1=Right 2=Ambidextrous)"""
-        return self['handedness']
-
-    # ---- Object access ------------------------------------------------- #
-    @property
-    def party(self):
-        """This character's party."""
-        return gamedata.party_by_id(self._partyid)
-
-    @property
-    def inventory(self):
-        """This character's inventory."""
-        return self[INVENTORY]
-
-    @property
-    def stats(self):
-        """This character's stats."""
-        return self[STATS]
-
-    @property
-    def statuses(self):
-        """This character's statuses."""
-        return self[STATUSES]
-
-    @property
-    def famtypes(self):
-        """This character's family types."""
-        return iter(self[BASE].famtypes)
-
-    @property
-    def base(self):
-        return self[BASE]
-
-    #@property
-    #def personality(self):
-        #return self['personality']
-
-    #@property
-    #def mood(self):
-        #return self['mood']
-
-    @property
-    def pplane(self):
-        """The current primary plane."""
-        return PLANES[self[PLANE][0]]
-
-    @property
-    def splane(self):
-        """The current secondary plane."""
-        return PLANES[self[PLANE][1]]
-
-    # ---- Tests --------------------------------------------------------- #
-    def bodytype(self):
-        return self.body.bodytype()
-
-    def is_alive(self):
-        return self.hp > 0
-
-    def hp(self):
-        return self[STATS][HEALTH]
-
-    def is_critical(self):
-        return self[STATS][HEALTH].is_critical()
-
-    def is_undead(self):
-        """True if this character is an undead type or has zombie status."""
-        return ("zombie" in self[STATUSES]["bools"] or
-            "zombie" in self.famtypes)
-
-    # ---- NPC and auto actions ------------------------------------------ #
-
-    @property
-    def alliance(self):
-        return self[ALLIANCE]
-
-    def _is_foe_of(self, x):
-        """True if x is a foe of this character."""
-        return False
-
-    def _is_neutral_of(self, x):
-        """True if x is neutral to this character."""
-        return True
-
-
-
-    # ---- Management ---------------------------------------------------- #
-    @property
-    def controller(self):
-        """This character's controller. ('ai', 'player', etc.)"""
-        return self._controller
-
-    def _set_controller(self, mode, party=None, req=False):
-        if mode != self.controller:
-            if mode == 'player':
-                self._controller = mode
-            elif mode == 'ai':
-                self._controller = mode
-            else:
-                # dont raise unless req=True
-                x = "Unknown controller mode '{}' for char._set_controller.".format(
-                    mode)
-                if req: raise(x)
-                print(x) # TODO should log this instead of print
-
-    def tempflags(self):
-        return self._tempflags
-
-    @property
-    def dflags(self):
-        """Died flags."""
-        return self['dflags']
-
-    @property
-    def ond_flags(self):
-        """On Died flags."""
-        return self['ond-flags']
-
-    @property
-    def onr_flags(self):
-        """On Revive flags."""
-        return self['onr-flags']
-
-    # ---- Event actions ------------------------------------------------- #
-    def died(self, *causes):
-        self.stats.health.hp = 0
-        for i in causes: self.dflags.add(i)
-
-
-    def revive(self, restore_amt=0):
-        # has no effect on any if restore_amt==0
-        if (restore_amt > 0 and 'null' not in self.onr_flags):
-            if not self.is_alive():
-                if not self.dflags.has_nonrevivable():
-
-                    self.dflags.clear()
-
-                    if 'half' in self.onr_flags:
-                        self.stats.health.hp = restore_amt/2
-                    elif '2x' in self.onr_flags:
-                        self.stats.health.hp = restore_amt*2
-                    else:
-                        self.stats.health.hp = restore_amt
-
-                    self.onr_flags.clear()
-                    self.on_revived(self)
-
-            # zombie checks
-            # TODO zombie famtype
-            elif self.statuses['undead'] > 0: # undead status
-                import random # TODO 'calc_factor' in restore_amt
-                n = random.randint(0, int(val)) # FIXME
-                if val > 50:
-                    x = bool(n in range(0, 100)) # FIXME
-                    if x: self.died('revived-as-zombie')
-                    else: pass # do n% dmg of health
-                else: # do n% dmg of health
-                    pass
-            else: pass # no effect
-
-
-    # ---- Events -------------------------------------------------------- #
-    def on_died(self, plane):
-        pass
-
-    def on_revived(self):
-        pass
-
-
-class Chars(dict):
-    """Character storage & management class."""
-    __slots__ = dict.__slots__
-
-
-class GameChars(Chars):
-    """Ingame character storage & management class."""
-    __slots__ = dict.__slots__
-
-
-    def npcs(self):
-        """Return an iterator of all NPCs (Non-playable characters)."""
-        return iter(i for i in self if i._npc)
-
-    def ai(self):
-        """Return an iterator of all ai controlled characters."""
-        return iter(i for i in self if i._controller == 'ai')
-
-
-class _Char_old:
-    """Base class for character objects."""
-    __slots__ = "_chardata", "_controller", "_owner"
-    def __init__(self, player, base):
-        self._owner =       owner
-        self._player =      owner
-        self._chardata =    CharData(self, base)
-
-    @property
-    def owner(self):
-        """The player who owns this character."""
-        return self._owner
-
-    @property
-    def player(self):
-        """The player currently controlling this character."""
-        return self._player
-
-    def owner_is_player(self):
-        """True if the owner of this character is also the one controlling it."""
-        return self._owner == self._player
-
-    def is_local_player(self):
-        """True if this char is controlled by the "player" object
-            on the local machine."""
-        return self._player == session.screen.player
 
+def _assemble(ob, dd, sd=stats_dict, st=statuses_dict):
+    ob._dataid = _get_new_charid(gdata, ob)
+    gamedata[ob._gdata][ob._dataid] = dd.copy()
+    gamedata[ob._gdata][ob._dataid]["stats"] = sd.copy()
+    gamedata[ob._gdata][ob._dataid]["statuses"] = st.copy()
+    #gamedict[ob._gdata][ob._dataid]["statusres"] = st.copy()
+    ob._data = gamedata[ob._gdata][ob._dataid]
+
+def _get_new_charid(gdata):
+    from random import randint
+    x = 0
+    while True:
+        if x not in gamedata[ob._gdata].keys(): break
+        x = randint(0, 9999999999999999999999)
+    return x
+
+def add_monster_dd(gdata, ob, dd=monster_data_dict): _assemble(ob, dd)
+def add_char_dd(gdata, ob, dd=chardata_dict): _assemble(ob, dd)
+def add_wildlife_dd(ob, dd=wildlife_datadict): _assemble(ob, dd)
+
+
+class _CharT: # character type base class
+    _ctid = 1 # chartype id -- 0 pc, 1 npc, 2 baddie (different from chartype)
+    is_baddie = False
+    __slots__ = ()
+    def is_npc(self): return True
+
+    def __destroy(self):
+        from chardata import gamedata
+        try:
+            del gamedata[self._gdata][self._dataid]
+            self._destroyed = True
+        except:
+            pass
+
+
+class _Baddie: # 'bad guy' mixin -- always NPC
+    _ctid = 2
+    is_baddie = True
+    __slots__ = ()
+
+
+# ---- Chartypes ------------------------------------------------------------- #
+class _Normal(_CharT):
+    _ctname = "normal"
+    __slots__ = ()
+    def __init__(self, gamedata):
+        from chardata import add_char_dd
+        self._destroyed = False
+        self._dataid = 0
+        self._data = add_char_dd(self)
+
+    def chartype(self): return 0
+    def has_tabby(self): return self._data["marker-flags"][0] > 0
+
+    # stuff TODO
+    def can_equip(self, item): pass
+    def is_in_guild(self, guild): pass
+
+
+class _WMCommon(_CharT): # mixin for stuff common to both _Wildlife and _Monster
+    __slots__ = _CharT.__slots__
+
+    def has_tabby(self): return False
+    def can_equip(self, item): return False
+    def is_in_guild(self, guild): return False
+
+
+class _Wildlife(_WMCommon):
+    _ctname = "wildlife"
+    __slots__ = _WMCommon.__slots__
+    def __init__(self):
+        from chardata import add_wildlife_dd
+        self._destroyed = False
+        self._dataid = 0
+        self._data = add_wildlife_dd(self)
+
+    def chartype(self): return 1
+
+
+class _Monster(_WMCommon): # may be playable in certain conditions
+    _ctname = "monster"
+    __slots__ = _WMCommon.__slots__
+    def __init__(self):
+        from chardata import add_monster_dd
+        self._destroyed = False
+        self._dataid =  0
+        self._data = add_monster_dd(self)
+
+    def chartype(self): return 2
+
+
+# ---- Baddie types ---------------------------------------------------------- #
+class CharBaddie(_Normal, _Baddie):
+    _gdata = "b0"
+    __slots__ = _Normal.__slots__
+
+
+class WildlifeBaddie(_Wildlife, _Baddie):
+    _gdata = "b1"
+    __slots__ = _Wildlife.__slots__
+
+
+class MonsterBaddie(_Monster, _Baddie):
+    _gdata = "b2"
+    __slots__ = _Monster.__slots__
+
+
+# ---- Npc types ------------------------------------------------------------- #
+class NPChar(_Normal)
+    _gdata = "np0"
+    __slots__ = _Normal.__slots__
+
+
+class NPCWildlife(_Wildlife):
+    _gdata = "np1"
+    __slots__ = _Wildlife.__slots__
+
+
+class NPCMonster(_Monster):
+    _gdata = "np2"
+    __slots__ = _Monster.__slots__
+
+
+# ---- Playable types -------------------------------------------------------- #
+class _PChar(_Char): # playable character mixin
+    _ctid = 0
+    __slots__ = _PChar.__slots__
+    def is_npc(self): return False
+
+
+class PChar(_Normal, _PChar)
+    _gdata = "pc0"
+    __slots__ = _Normal.__slots__
+
+
+class PCWildlife(_Wildlife, _PChar):
+    _gdata = "pc1"
+    __slots__ = _Wildlife.__slots__
+
+
+class PCMonster(_Monster, _PChar):
+    _gdata = "pc2"
+    __slots__ = _Monster.__slots__
 
